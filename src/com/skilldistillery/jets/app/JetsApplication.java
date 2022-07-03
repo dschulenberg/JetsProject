1300ee7cfa803d495fd1107835fe50966a682904
Daniel Schulenberg <dschumil@gmail.com>
2022-07-03 16:21:38 -0700

    Most likely the final submission

--- src/com/skilldistillery/jets/app/JetsApplication.java
+++ src/com/skilldistillery/jets/app/JetsApplication.java
@@ -1,13 +1,8 @@
 package com.skilldistillery.jets.app;
 
-import java.util.List;
 import java.util.Scanner;
 
 import com.skilldistillery.jets.entities.AirField;
-import com.skilldistillery.jets.entities.CargoJet;
-import com.skilldistillery.jets.entities.FighterJet;
-import com.skilldistillery.jets.entities.Jet;
-import com.skilldistillery.jets.entities.Passenger;
 
 public class JetsApplication {
 	private Scanner scan = new Scanner(System.in);
@@ -30,53 +25,87 @@ public class JetsApplication {
 	}
 
 	public static void main(String[] args) {
+		JetsApplication ja = new JetsApplication();
+		ja.menuLoop();
+
+	}
+
+	public void menuLoop() {
+		boolean start = true;
 		AirField af = new AirField();
 		af.launch();
-		af.listFleet();
-	//	 af.loadCargoJets();
-		 af.fightFighterJets();
-	
-	//	ja.removeJet(currentFleet);
-	//	af.addJet();
-	//	af.removeJet();
+
+		do {
+			System.out.println();
+			displayMenu();
+			System.out.println();
+			int choice = 0;
+			try {
+
+				choice = scan.nextInt();
+
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				System.out.println("Please enter a number.");
+				scan.nextLine();
+			}
+			switch (choice) {
+
+			case 1:
+				af.listFleet();
+				break;
+			case 2:
+				af.flyAll();
+				break;
+			case 3:
+				af.listFastest();
+				break;
+			case 4:
+				af.listLongest();
+				break;
+			case 5:
+				af.loadCargoJets();
+				break;
+			case 6:
+				af.fightFighterJets();
+				break;
+			case 7:
+				af.addJet();
+				break;
+			case 8:
+				af.removeJet();
+				break;
+			case 9:
+				System.out.println("Thank you for using our app. Goodbye");
+				start = false;
+				break;
+			default:
+				System.out.println("Error. Please enter a listed number.");
+				break;
+
+			}
+		} while (start);
+
+		scan.close();
 
 	}
 
-
-
-	public void listFastest(List<Jet> currentFleet) {
-		int highest = 0;
-		for (Jet each : currentFleet) {
-			if (each.getSpeed() > highest) {
-				highest = each.getSpeed();
-			}
-		}
-		for (Jet each : currentFleet) {
-			if (each.getSpeed() == highest) {
-				System.out.println(each);
-			}
-		}
-	}
-
-	public void listLongest(List<Jet> currentFleet) {
-		int longest = 0;
-		for (Jet each : currentFleet) {
-			if (each.getRange() > longest) {
-				longest = each.getRange();
-			}
-		}
-		for (Jet each : currentFleet) {
-			if (each.getRange() == longest) {
-				System.out.println(each);
-			}
-		}
-	}
-
-
-	public void flyAll(List<Jet> currentFleet) {
-		for (Jet each : currentFleet) {
-			each.fly();
-		}
+	public void displayMenu() {
+		char p = '\u2708';
+		System.out.println(p + "  " + p + "  " + p + "  " + p + "  " + p + "  " + p + "  " + p + "  " + p + "  " + p
+				+ "  " + p + "  " + p + "  " + p + "  " + p + "  ");
+		System.out.println("Please choose a number from the following:");
+		System.out.println("1. List fleet");
+		System.out.println("2. Fly all jets");
+		System.out.println("3. View fastest jet");
+		System.out.println("4. View jet with longest range");
+		System.out.println("5. Load all Cargo Jets");
+		System.out.println("6. Dogfight!");
+		System.out.println("7. Add a jet to Fleet");
+		System.out.println("8. Remove a jet from Fleet");
+		System.out.println("9. Quit");
+		System.out.println(p + "  " + p + "  " + p + "  " + p + "  " + p + "  " + p + "  " + p + "  " + p + "  " + p
+				+ "  " + p + "  " + p + "  " + p + "  " + p + "  ");
 
 	}
 
--- src/com/skilldistillery/jets/entities/AirField.java
+++ src/com/skilldistillery/jets/entities/AirField.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import com.skilldistillery.jets.app.JetsApplication;
+
 public class AirField {
 	public List<Jet> getFleet() {
 		return createFleet(fleet);
@@ -16,100 +18,125 @@ public class AirField {
 		this.fleet = fleet;
 	}
 
-	private List<Jet> fleet =new ArrayList<>();
-	
-	public List<Jet> createFleet(List<Jet> fleet){
-	   try (BufferedReader bufIn = new BufferedReader(new FileReader("jets.txt"))) {
-	        String line;
-	        while ((line = bufIn.readLine()) != null) {
-	          String[] jetRecord = line.split(",");
-	          String type = jetRecord[0];
-	          String model = jetRecord[1];
-	          int speed = Integer.parseInt(jetRecord[2]);
-	          int range = Integer.parseInt(jetRecord[3]);
-	          double price = Double.parseDouble(jetRecord[4]);
-	          if(type.equals("cargo")) {
-	        	  Jet c = new CargoJet(model, speed,range,price);
-	        	 fleet.add(c);
-	        	  
-	          }
-	          if(type.equals("fighter")) {
-	        	  Jet f = new FighterJet(model, speed,range,price);
-	        	  fleet.add(f);
-	          }
-	          if(type.equals("passenger")) {
-	          Jet p = new Passenger(model, speed,range,price);
-	          fleet.add(p);
-	          }
-	          
-	        }
-	      }
-	      catch (IOException e) {
-	        System.err.println(e);
-	      } 
-	   return fleet;
+	private List<Jet> fleet = new ArrayList<>();
+
+	public List<Jet> createFleet(List<Jet> fleet) {
+		try (BufferedReader bufIn = new BufferedReader(new FileReader("jets.txt"))) {
+			String line;
+			while ((line = bufIn.readLine()) != null) {
+				String[] jetRecord = line.split(",");
+				String type = jetRecord[0];
+				String model = jetRecord[1];
+				int speed = Integer.parseInt(jetRecord[2]);
+				int range = Integer.parseInt(jetRecord[3]);
+				double price = Double.parseDouble(jetRecord[4]);
+				if (type.equals("cargo")) {
+					Jet c = new CargoJet(model, speed, range, price);
+					fleet.add(c);
+
+				}
+				if (type.equals("fighter")) {
+					Jet f = new FighterJet(model, speed, range, price);
+					fleet.add(f);
+				}
+				if (type.equals("passenger")) {
+					Jet p = new Passenger(model, speed, range, price);
+					fleet.add(p);
+				}
+
+			}
+		} catch (IOException e) {
+			System.err.println(e);
+		}
+		return fleet;
 	}
+
 	public void launch() {
 		getFleet();
 	}
+
 	public void listFleet() {
-		
+
 		for (Jet each : fleet) {
 			System.out.println(each);
 		}
 
 	}
-	public void removeJet() {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Would you like to remove by 'number' or 'model'?");  //User must type in the string number or model.
-		System.out.println();
-		String userInput = scan.nextLine();
-		if(userInput.equals("number")) {
-			System.out.println("Which jet would you like to remove by number?");
-			System.out.println();
+
+	public void flyAll() {
 		for (Jet each : fleet) {
-			System.out.println("Number: " + fleet.indexOf(each) + " "+each);
+			each.fly();
 		}
-		int numRemoved = scan.nextInt();
-		fleet.remove(numRemoved);
-		}
-		if(userInput.equals("model")) {
-			System.out.println("Which jet would you like to remove by Model?");
-			System.out.println();
-			for (Jet each : fleet) {
-				System.out.println("Number: " + fleet.indexOf(each) + " "+each);
-			}
-		String modRemoved = scan.nextLine();
-		int index = 0;
-		for (Jet each : fleet) {
-			if(each.getModel().equals(modRemoved)) {
-				break;
-			}
-			index++;
-		}
-		fleet.remove(index);
-		}
-		
-		scan.close();
+
 	}
-	
+
+	public void listFastest() {
+		int highest = 0;
+		for (Jet each : fleet) {
+			if (each.getSpeed() > highest) {
+				highest = each.getSpeed();
+			}
+		}
+		for (Jet each : fleet) {
+			if (each.getSpeed() == highest) {
+				System.out.println(each);
+			}
+		}
+	}
+
+	public void listLongest() {
+		int longest = 0;
+		for (Jet each : fleet) {
+			if (each.getRange() > longest) {
+				longest = each.getRange();
+			}
+		}
+		for (Jet each : fleet) {
+			if (each.getRange() == longest) {
+				System.out.println(each);
+			}
+		}
+	}
+
+	public void loadCargoJets() {
+		for (Jet each : fleet) {
+			if (each instanceof CargoJet) {
+				((CargoJet) each).loadCargo();
+			}
+		}
+
+	}
+
+	public void fightFighterJets() {
+		for (Jet each : fleet) {
+			if (each instanceof FighterJet) {
+				((FighterJet) each).fight();
+			}
+		}
+
+	}
+
 	public void addJet() {
 		char p = '\u2708';
 		boolean start = true;
 		Jet newJet = null;
-		Scanner scan = new Scanner(System.in);
-
+		JetsApplication ja = new JetsApplication();
+		Scanner scan = ja.getScan();
+		
 		System.out.println("Enter the model of your Jet.");
 		String model = scan.nextLine();
 
 		System.out.println("Enter the speed of your Jet.");
 		int speed = scan.nextInt();
+		
 		System.out.println("Enter the range of your Jet.");
 		int range = scan.nextInt();
+		
 		System.out.println("Enter the price of your Jet.");
 		double price = scan.nextDouble();
-		System.out.println();
 		
+		System.out.println();
+
 		while (start) {
 			System.out.println(p + "  " + p + "  " + p + "  " + p + "  " + p + "  " + p + "  " + p + "  " + p + "  " + p
 					+ "  " + p + "  " + p + "  " + p + "  " + p + "  ");
@@ -120,7 +147,7 @@ public class AirField {
 			System.out.println(p + "  " + p + "  " + p + "  " + p + "  " + p + "  " + p + "  " + p + "  " + p + "  " + p
 					+ "  " + p + "  " + p + "  " + p + "  " + p + "  ");
 			int choice = scan.nextInt();
-		
+
 			switch (choice) {
 			case 1:
 				newJet = new CargoJet(model, speed, range, price);
@@ -139,24 +166,71 @@ public class AirField {
 				break;
 			}
 		}
-		scan.close();
 		fleet.add(newJet);
+		
 	}
-	public void loadCargoJets() {
-		for (Jet each : fleet) {
-			if (each instanceof CargoJet) {
-				((CargoJet) each).loadCargo();
-			}
-		}
 
-	}
-	public void fightFighterJets() {
-		for (Jet each : fleet) {
-			if (each instanceof FighterJet) {
-				((FighterJet) each).fight();
-			}
+	public void removeJet() {
+		JetsApplication ja = new JetsApplication();
+		Scanner scan = ja.getScan();
+		boolean removeMethod = true;
+		if (fleet.size() == 0) {
+			System.out.println("No more Jets. Please add more before removing more.");
+			return;
+
 		}
+		do {
+			System.out.println("Would you like to remove by 'number' or 'model'?"); // User must type in the string
+																					// number or model.
+			System.out.println();
+			String userInput = scan.nextLine();
+			if (!userInput.equals("number") && !userInput.equals("model")) {
+				System.out.println("ERROR!! You must type either 'model' or 'number' to start.");
+			}
+
+			if (userInput.equals("number")) {
+				System.out.println("Which jet would you like to remove by number?");
+				System.out.println();
+				for (Jet each : fleet) {
+					System.out.println("Number: " + fleet.indexOf(each) + " " + each);
+				}
+
+				try {
+					int numRemoved = scan.nextInt();
+					fleet.remove(numRemoved);
+					removeMethod = false;
+				} catch (Exception e) {
+					System.out.println("You must enter the number of a jet listed.");
+					scan.nextLine();
+					System.out.println();
+				}
+			}
+			else if (userInput.equals("model")) {
+				System.out.println("Which jet would you like to remove by Model?");
+				System.out.println();
+				for (Jet each : fleet) {
+					System.out.println(each);
+				}
+				try {
+					int index = 0;
+					String modRemoved = scan.nextLine();
+					for (Jet each : fleet) {
+						if (each.getModel().equals(modRemoved)) {
+							break;
+						}
+						index++;
+					}
+					fleet.remove(index);
+					removeMethod = false;
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					System.out.println("You must enter the model of a jet listed.");
+					System.out.println();
+				}
+
+			}
+
+		} while (removeMethod);
 
 	}
 }
-
